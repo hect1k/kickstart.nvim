eb648e48c41c102d693cbcd3689f4a24b7d8308a
Nnisarg Gada <contact@nnisarg.in>
2025-10-21 21:56:12 +0530

    fixes

--- init.lua
+++ init.lua
@@ -928,19 +928,24 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('gruvbox').setup()
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox'
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_italic = true
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'gruvbox-material'
       vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { link = 'LineNr' })
-      vim.cmd 'hi! Normal guibg=NONE ctermbg=NONE'
-      vim.cmd 'hi! NonText guibg=NONE ctermbg=NONE'
+
+      -- Transparent background
+      -- vim.cmd 'hi! Normal guibg=NONE ctermbg=NONE'
+      -- vim.cmd 'hi! NonText guibg=NONE ctermbg=NONE'
     end,
   },
 
